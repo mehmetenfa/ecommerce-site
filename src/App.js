a1d6860751d2a38a19c0fb49a2ae8cecea542c8d
qxan <mehmet.enfa@gmail.com>
2023-03-20 11:50:10 +0300

    app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Footer } from "./components/footer/Footer";
+import { Header } from "./components/header/Header";
+import { Account } from "./pages/account/Account";
+import { Home } from "./pages/home/Home";
+import { Login } from "./pages/login/Login";
+import { Register } from "./pages/login/Register";
+import { useSelector } from "react-redux";
 
-function App() {
+const App = () => {
+  const isLoggIn = useSelector((state) => state.auth.isLoggIn);
+  const cartItems = useSelector((state) => state.cart.itemsList);
+  console.log(cartItems);
   return (
-    <div className="App">
-      whats up nigga
-    </div>
+    <>
+      {isLoggIn && (
+        <Router>
+          <Header />
+          <Routes>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/account" component={Account} />
+          </Routes>
+          <Footer />
+        </Router>
+      )}
+      {!isLoggIn && <Login />}
+    </>
   );
-}
-
+};
 export default App;
