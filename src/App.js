667a2d11f8ac690363039c2df0192107241df68b
qxan <mehmet.enfa@gmail.com>
2023-03-21 12:24:08 +0300

    update register

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Footer } from "./components/footer/Footer";
-import { Header } from "./components/header/Header";
-import { Account } from "./pages/account/Account";
-import { Home } from "./pages/home/Home";
-import { Login } from "./pages/login/Login";
-import { Register } from "./pages/login/Register";
-import { useSelector } from "react-redux";
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Footer } from "./components/footer/Footer"
+import { Header } from "./components/header/Header"
+import { Account } from "./pages/account/Account"
+import { Home } from "./pages/home/Home"
+import { Login } from "./pages/login/Login"
+import { Regsiter } from "./pages/login/Regsiter"
+import { useSelector } from "react-redux"
 
 const App = () => {
-  const isLoggIn = useSelector((state) => state.auth.isLoggIn);
-  const cartItems = useSelector((state) => state.cart.itemsList);
-  console.log(cartItems);
+  const isLoggIn = useSelector((state) => state.auth.isLoggIn)
+  const cartItems = useSelector((state) => state.cart.itemsList)
+  console.log(cartItems)
   return (
     <>
       {isLoggIn && (
         <Router>
           <Header />
-          <Routes>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/account" component={Account} />
-          </Routes>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/regsiter' component={Regsiter} />
+            <Route exact path='/account' component={Account} />
+          </Switch>
           <Footer />
         </Router>
       )}
       {!isLoggIn && <Login />}
     </>
-  );
-};
-export default App;
+  )
+}
+export default App
