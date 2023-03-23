eca0f0e2f3ae226ea6afba9e0a74ec2e5f23aedb
qxan <mehmet.enfa@gmail.com>
2023-03-23 15:03:04 +0300

    Slider

--- src/components/hero/Slider.jsx
+++ src/components/hero/Slider.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./slider.css";
+import { slide } from "../../assets/data/data";
 
-export const Slider = () => {
+export const Order = () => {
   return (
-    <div>Slider</div>
-  )
-}
+    <>
+      <div className="slider">
+        <div className="container grid">
+          {slide.map((item, i) => (
+            <div className="box" key={i}>
+              <div className="img">
+                <img src={item.image} alt="" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
